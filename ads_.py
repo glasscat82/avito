bfac85d3ab93b83d1d8f29a6dccd2fee80b0f3a8
glasscat82 <glasscat82@gmail.com>
2022-10-16 11:52:11 +0200

    🍩

---
+++ ads_.py
@@ -0,0 +1,36 @@
+# parsing avito.ru for Russia
+from datetime import datetime
+# from prettytable import PrettyTable
+from avito import avito
+from pprint import pprint
+from prettytable import PrettyTable 
+
+def main():
+    start = datetime.now()	
+
+    # ----- return ads html
+    url_ = 'https://www.avito.ru/kaliningrad/noutbuki/lenovo-ASgCAQICAUCo5A0U5tlm?cd=1&s=1&user=1'
+    ads = avito(url=url_, filename='./json/avito.json')
+    html_ = ads.get_html()
+    # ads.p(html_)
+    links_ = ads.get_all_links(html=html_)
+    # add json
+    ads.write_json(data=links_)
+    if len(links_):
+        # the table        
+        x = PrettyTable()
+        x.field_names = ["№", "Заголовок", "Пользователь", "Место", "Цена", "Время"]
+        x.align["Заголовок"] = x.align["Описание"] = x.align["Пользователь"] = "l"
+        x.align["Время"] = x.align["Место"] = "l"
+        for index, r_ in enumerate(links_, 1):
+            x.add_row([index, r_[1], r_[7], r_[5], r_[2], r_[4]])
+        print(x.get_string(title = str(len(links_)) +' объявлений'))
+    else:
+        print(ads.error)
+    # ----- end ads
+
+    end = datetime.now()
+    print(str(end-start))
+
+if __name__ == '__main__':
+	main()
--- avito.py
+++ avito.py
@@ -10,6 +10,7 @@ class avito():
     def __init__(self, url="https://www.avito.ru/kaliningrad/noutbuki?cd=1&s=1&user=1", filename="avito.json"):
         self.url = url
         self.filename = filename
+        self.error = []
 
     @staticmethod
     def p(text, *args):
@@ -44,40 +45,44 @@ class avito():
             return False
         soup = BeautifulSoup(html, 'lxml')
         selection_list = soup.find('div', {'data-marker':'catalog-serp'})    
+        # self.p(soup.find('h1').text.strip())
         # self.p(selection_list)
         
         links = []
-        for r_ in selection_list.find_all('div', {'data-marker':'item'}):
-            # add item ads
-            try:
-                item_ = r_.find('a', {'data-marker':'item-title'})
-                item_a = item_.get('href')
-                item_name = item_.text.strip()
-                prise = r_.find('span', {'data-marker':'item-price'})
-                item_prise = prise.find('meta', {'itemprop':'price'}).get('content')
-                item_currency = prise.find('meta', {'itemprop':'priceCurrency'}).get('content')
-                item_date = r_.find('div', {'class':'date-root-QeIIB'}).text.strip()
-                item_description = r_.find('meta', {'itemprop':'description'}).get('content').strip()
-                item_adress = r_.find('div', {'class':'geo-root-H3eWU'}).text.strip()
-                user_ = r_.find('a', {'data-marker':'item-link'})
-                item_user = user_.text.strip()
-                right_ = r_.find('div', {'class':'iva-item-aside-c_vio'})
-                items_span = ", ".join([x.text.strip() for x in right_.find_all('span', {'class':'text-text-LurtD'})])
-                
+        if selection_list is not None:
+            for r_ in selection_list.find_all('div', {'data-marker':'item'}):
+                # add item ads
+                try:
+                    item_ = r_.find('a', {'data-marker':'item-title'})
+                    item_a = item_.get('href')
+                    item_name = item_.text.strip()
+                    prise = r_.find('span', {'data-marker':'item-price'})
+                    item_prise = prise.find('meta', {'itemprop':'price'}).get('content')
+                    item_currency = prise.find('meta', {'itemprop':'priceCurrency'}).get('content')
+                    item_date = r_.find('div', {'class':'date-root-QeIIB'}).text.strip()
+                    item_description = r_.find('meta', {'itemprop':'description'}).get('content').strip()
+                    item_adress = r_.find('div', {'class':'geo-root-H3eWU'}).text.strip()
+                    user_ = r_.find('a', {'data-marker':'item-link'})
+                    item_user = user_.text.strip()
+                    right_ = r_.find('div', {'class':'iva-item-aside-c_vio'})
+                    items_span = ", ".join([x.text.strip() for x in right_.find_all('span', {'class':'text-text-LurtD'})])
+                    
 
-                row = []
-                row.append(item_a)
-                row.append(item_name)
-                row.append(item_prise)
-                row.append(item_currency)                
-                row.append(item_date)
-                row.append(item_adress)
-                row.append(item_description)
-                row.append(item_user)
-                row.append(items_span)
-                links.append(row)
-            except Exception as e:
-                print(sys.exc_info()[1])
-                continue
-                
+                    row = []
+                    row.append(item_a)
+                    row.append(item_name)
+                    row.append(item_prise)
+                    row.append(item_currency)                
+                    row.append(item_date)
+                    row.append(item_adress)
+                    row.append(item_description)
+                    row.append(item_user)
+                    row.append(items_span)
+                    links.append(row)
+                except Exception as e:
+                    print(sys.exc_info()[1])
+                    continue
+        else:
+            self.error.append(soup.find('h1').text.strip())
+        
         return links
